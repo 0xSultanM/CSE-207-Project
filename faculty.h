eec7410ab474a00bd9434cd87f76013f7c03a887
0xSultanM <sultanmprince@gmail.com>
2025-07-24 18:05:24 +0600

    Adding faculty APIs

--- faculty.h
+++ faculty.h
@@ -1,5 +1,17 @@
 #ifndef FACULTY_H
 #define FACULTY_H
 
+enum designation{
+    LECTURER = 0;
+    ASSISTANT_PROFESSOR,
+    ASSOCIATE_PROFESSOR,
+    PROFESSOR,
+}
+
+typedef struct faculty{
+    char *name;
+    int *id;
+    char *designation;
+} faculty;
 
 #endif
