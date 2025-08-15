03b41e1ff49bd272aff76a0f8cdb34161a06eb78
0xSultanM <sultanmprince@gmail.com>
2025-08-15 22:38:07 +0600

    Updated Code for Admin.C File

---
+++ admin.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include"admin.h"
+
+// declaring the structure for exam schedule
+struct examSchedule exam;
+
+//fuctions for exam schedule
+void addExamSchedule(){
+    printf("\t\tWrite the course code: ");
+    scanf("%s",&exam.courseCode);
+    printf("\t\tType the Date ( Format: DD/MM/YEAR ): ");
+    scanf("%s",&exam.date);
+    printf("\t\tType the Date ( Format: 10:00 AM ): ");
+    scanf("%s",&exam.time);
+};
+void editExamSchedule();
+void viewExamSchedule();
+void deleteExamSchedule();
+int isPublishedES();
+
+// declaring the structure for grade
+struct gradeDetails grades;
+
+// functions for grade
+void viewGrade();
+void editGrade();
+void publishGrade();
+int isPublishGrade();
---
+++ admin.h
@@ -0,0 +1,35 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+//write all the functions in here
+
+
+//  ~~~~ Exam and Grades ~~~~
+
+// Exam schedule
+typedef struct examSchedule {
+    char courseCode[12];
+    char date[12];
+    char time[10];
+};
+
+void addExamSchedule();
+void editExamSchedule();
+void viewExamSchedule();
+void deleteExamSchedule();
+int isPublishedES();
+
+// Grade Details
+struct gradeDetails{
+    char studentID[16];
+    char courseCode[12];
+    char grade[4];
+};
+
+
+void viewGrade();
+void editGrade();
+void publishGrade();
+int isPublishGrade();
+
+#endif
