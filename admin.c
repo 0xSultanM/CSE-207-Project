05be7e48e18e42e976564a47879829711c66a4e5
0xSultanM <sultanmprince@gmail.com>
2025-08-15 23:51:57 +0600

    Updated version for Edit Exam Function

--- admin.c
+++ admin.c
@@ -11,12 +11,25 @@ void addExamSchedule(){
     scanf("%s",&exam.courseCode);
     printf("\t\tType the Date ( Format: DD/MM/YEAR ): ");
     scanf("%s",&exam.date);
-    printf("\t\tType the Date ( Format: 10:00 AM ): ");
+    printf("\t\tType the Time ( Format: 10:00 AM ): ");
     scanf("%s",&exam.time);
 };
 void editExamSchedule(){
+    char c,prev;
     printf("Write the course code: ");
-    scanf("%s",&exam.courseCode);
+    scanf("%s",&c);
+    int n = malloc(exam);
+    for(int i=0;i<n;i++){
+        prev = exam.courseCode[i];
+        if(strcmp(c,prev ) == 0){
+            printf("\t\tType the New Date ( Format: DD/MM/YEAR ): ");
+            scanf("%s",&exam.date);
+            printf("\t\tType the New Time ( Format: 10:00 AM ): ");
+            scanf("%s",&exam.time);
+        } else {
+            printf("There isn't any scheduled exam for %s course",exam.courseCode);
+        }
+    }
 };
 void viewExamSchedule();
 void deleteExamSchedule();
